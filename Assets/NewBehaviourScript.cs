2d2b06b08193fb4f69836d1dd5062c426f3a571a
dazimo <dazimo@gmail.com>
2016-03-12 15:24:34 +0900

    첫 저장

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    void OnMouseDown()
+    {
+        gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
